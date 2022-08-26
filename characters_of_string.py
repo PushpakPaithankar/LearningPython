74a441f1368677c1186550392263ff187d82a17f
P-u-s-h-p-a-k <pushpakrp197@gmail.com>
2022-08-26 21:23:25 +1000

    while_loop program_corrected

--- characters_of_string.py
+++ characters_of_string.py
@@ -12,4 +12,11 @@ while (i<len(a)):
     print (a[i])
     i=i+1
 
-print(f"Reverse order is {(a[::-1])}")
+print(f"Your reverse string of {a} is: ")
+i=0
+
+# Reverse of previous string
+b=a[::-1]
+while (i<len(b)):
+    print (b[i])
+    i=i+1
