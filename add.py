6f0214e92134446b3804f198f92a1c5e97b42cc0
P-u-s-h-p-a-k <pushpakrp197@gmail.com>
2022-08-26 10:49:46 +1000

    add program

---
+++ add.py
@@ -0,0 +1,11 @@
+# input
+a= int(input("Enter a number: "))
+b= int(input("Enter another number: "))
+
+# define a function to add
+def add(a, b):
+    return (a + b)
+c = add(a,b)
+
+# print
+print(f"The sum of {a} and {b} is {c}")
